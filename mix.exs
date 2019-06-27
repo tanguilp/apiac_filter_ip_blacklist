d243efe18014b722aebbe91ef1817838f4dcecb0
Tangui <29804907+tanguilp@users.noreply.github.com>
2019-06-27 21:41:50 +0300

    Minor fix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule APIacFilterIPBlacklist.MixProject do
   def project do
     [
       app: :apiac_filter_ip_blacklist,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
